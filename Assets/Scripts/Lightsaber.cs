53b8d9b687b3abd3d2b87205f4421c8d2d19ff11
whaoran <whaoran@seas.upenn.edu>
2019-11-25 18:32:48 -0500

    slicing

---
+++ Assets/Scripts/Lightsaber.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Lightsaber : MonoBehaviour
+{
+    public SteamVR_Controller.Device device;
+
+    [HideInInspector]
+    public Interaction interaction;
+
+    public void Setup(SteamVR_Controller.Device newDevice, Interaction newInteraction)
+    {
+        device = newDevice;
+        interaction = newInteraction;
+    }
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Obstacle : MonoBehaviour {
+    public GameObject obj1;
+    public GameObject obj2;
+
+    private Collider collider = null;
+
+    void Awake() {
+        collider = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        // Is it a lightsaber
+        if (!collider.gameObject.CompareTag("Lightsaber")) {
+            return;
+        }
+
+        Lightsaber lightSaber = collider.gameObject.GetComponent<Lightsaber>();
+
+        // Is the lightsaber in use?
+        if (lightSaber.device == null) {
+            return;
+        }
+
+        // Check for split
+        Split(lightSaber);
+    }
+
+    private void Split(Lightsaber lightSaber) {
+        // Split obstacle
+        if (lightSaber.device.velocity.magnitude > 3.0f) {
+            // Disable collision, so we only split once
+            collider.enabled = false;
+
+            // Enable physics for both sides
+            EnablePhysics(obj1);
+            EnablePhysics(obj2);
+        }
+    }
+
+    private void EnablePhysics(GameObject obstacle) {
+        obj1.transform.parent = null;
+
+        Rigidbody rb = log.GetComponent<RigidBody>();
+
+        rb.useGravity = true;
+        rb.isKinematic = false;
+    }
+}
