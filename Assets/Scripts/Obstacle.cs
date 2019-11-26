6b3c17d3a9047a645544f3f849ddc09f74c78349
Prutha <pruthadubhashi@gmail.com>
2019-11-25 19:12:40 -0500

    slicing2

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -7,45 +7,63 @@ public class Obstacle : MonoBehaviour {
     public GameObject obj1;
     public GameObject obj2;
 
-    private Collider collider = null;
+    //private Collider collider = null;
 
     void Awake() {
-        collider = GetComponent<Collider>();
+        //collider = GetComponent<Collider>();
     }
 
-    void OnTriggerEnter(Collider collider) {
-        // Is it a lightsaber
-        if (!collider.gameObject.CompareTag("Lightsaber")) {
-            return;
-        }
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Collision");
 
-        Lightsaber lightSaber = collider.gameObject.GetComponent<Lightsaber>();
+        // Is it a lightsaber
+        //if (other.CompareTag("Lightsaber")) {
+            Debug.Log("Lightsaber");
+            Split();
+        //}
+
+        
+
+        //Lightsaber lightSaber = collider.gameObject.GetComponent<Lightsaber>();
 
         // Is the lightsaber in use?
-        if (lightSaber.device == null) {
-            return;
-        }
+        //if (lightSaber.device == null) {
+        //    return;
+        //}
 
         // Check for split
-        Split(lightSaber);
+        //Split(lightSaber);
+
+        //Split();
     }
 
-    private void Split(Lightsaber lightSaber) {
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("Collision");
+        if(collision.collider.CompareTag("Lightsaber"))
+        {
+            Debug.Log("Light Saber");
+            Split();
+        }
+    }
+
+    //private void Split(Lightsaber lightSaber) {
+    private void Split() { 
         // Split obstacle
-        if (lightSaber.device.velocity.magnitude > 3.0f) {
+        //if (lightSaber.device.velocity.magnitude > 3.0f) {
             // Disable collision, so we only split once
-            collider.enabled = false;
+            GetComponent<Collider>().enabled = false;
 
             // Enable physics for both sides
             EnablePhysics(obj1);
             EnablePhysics(obj2);
-        }
+        //}
     }
 
-    private void EnablePhysics(GameObject obstacle) {
+    private void EnablePhysics(GameObject obj) {
         obj1.transform.parent = null;
 
-        Rigidbody rb = log.GetComponent<RigidBody>();
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
 
         rb.useGravity = true;
         rb.isKinematic = false;
